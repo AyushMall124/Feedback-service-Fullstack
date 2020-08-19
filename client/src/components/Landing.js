e81187ec73de41826047bd1ae3dbb490e62ea79a
Ayush Mall <ayushmall124@gmail.com>
2020-08-19 21:11:19 +0530

    User can see the surveys now

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Feedback Services</h1>
       Send emails to your users to collect feedback
+      <div style={{ marginTop: "15px" }}>
+        <Link to="/surveys" className=" waves-effect waves-light btn center">
+          Get Started
+        </Link>
+      </div>
     </div>
   );
 };
